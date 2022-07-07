7caaaff483372f0758d3f7a7c3ddd329e2606d6d
Abdulhamid <adioabdulhamid@yahoo.com>
2022-07-07 15:16:51 +0100

    Use html5 sematic tags

--- src/components/Books.js
+++ src/components/Books.js
@@ -4,12 +4,16 @@ import Form from './Form';
 
 function Books() {
   return (
-    <>
-      <Book title="The Hunger Games" author="Suzanne Collins" percentageCompleted={64} currentChapter={17} />
-      <Book title="Dune" author="Frank Herbert" percentageCompleted={8} currentChapter={3} />
-      <Book title="Capital in the Twenty-First Century" author="Suzanne Collins" percentageCompleted={0} currentChapter="Introduction" />
-      <Form />
-    </>
+    <main>
+      <section>
+        <Book title="The Hunger Games" author="Suzanne Collins" percentageCompleted={64} currentChapter={17} />
+        <Book title="Dune" author="Frank Herbert" percentageCompleted={8} currentChapter={3} />
+        <Book title="Capital in the Twenty-First Century" author="Suzanne Collins" percentageCompleted={0} currentChapter="Introduction" />
+      </section>
+      <section>
+        <Form />
+      </section>
+    </main>
   );
 }
 
