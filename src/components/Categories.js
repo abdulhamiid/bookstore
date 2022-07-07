86d4a8f586354cbfbbe95cd6de8969e3bc0cb5d9
Abdulhamid <adioabdulhamid@yahoo.com>
2022-07-07 15:04:46 +0100

    Add categories page component

---
+++ src/components/Categories.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Nav from './Nav';
+
+function Categories() {
+  return (
+    <>
+      <Nav />
+      <button type="button">Check Status</button>
+    </>
+  );
+}
+
+export default Categories;
