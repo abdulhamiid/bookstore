8cd964dcdb196495db4e57c73f05c3477604a51f
Abdulhamid <adioabdulhamid@yahoo.com>
2022-07-07 15:10:54 +0100

    Remove nav component

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import Book from './Book';
 import Form from './Form';
-import Nav from './Nav';
 
 function Books() {
   return (
     <>
-      <Nav />
       <Book title="The Hunger Games" author="Suzanne Collins" percentageCompleted={64} currentChapter={17} />
       <Book title="Dune" author="Frank Herbert" percentageCompleted={8} currentChapter={3} />
       <Book title="Capital in the Twenty-First Century" author="Suzanne Collins" percentageCompleted={0} currentChapter="Introduction" />
