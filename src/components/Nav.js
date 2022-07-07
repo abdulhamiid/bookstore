5a78f7391581231a5b91c97d691409a2267b23a4
Abdulhamid <adioabdulhamid@yahoo.com>
2022-07-07 15:21:46 +0100

    Refactor to html5 sematic tags

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -8,10 +8,14 @@ function Nav() {
   return (
     <nav>
       <h1>Bookstore CMS</h1>
-      <div className="navbar">
-        <Link to="/">BOOKS</Link>
-        <Link to="/categories">CATEGORIES</Link>
-      </div>
+      <ul className="navbar">
+        <li>
+          <Link to="/">BOOKS</Link>
+        </li>
+        <li>
+          <Link to="/categories">CATEGORIES</Link>
+        </li>
+      </ul>
     </nav>
   );
 }
