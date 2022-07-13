ed26a2ff6fa55d44dc483b91e8f5c856c94cc94b
Abdulhamid <adioabdulhamid@yahoo.com>
2022-07-13 18:39:51 +0100

    Add uuidv4 library to generate book id

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,46 +1,41 @@
+import { v4 as uuidv4 } from "uuid";
 // Actions
 
-const ADD_BOOK   = 'bookstore/books/ADD_BOOK';
-const REMOVE_BOOK = 'bookstore/books/ADD_BOOK'
+const ADD_BOOK = 'bookstore/books/ADD_BOOK';
+const REMOVE_BOOK = 'bookstore/books/ADD_BOOK';
 
-let nextBook = 0
 // Reducer
-export default function reducer(state = [], action = {}) {
+export default function bookReducer(state = [], action) {
   switch (action.type) {
     case ADD_BOOK:
       return [
         ...state,
         {
-          id: action.id,
+          id: uuidv4(),
           book: action.book,
-          completed: false
-        }
-      ]
+        },
+      ];
+
     case REMOVE_BOOK:
-      return state.filter(item => state[action.id-1].id != item.id )
+      return state.filter((item) => action.id !== item.id);
+      
     default:
-      return state
+      return state;
   }
 }
 
 // Action Creators
 export function addBook(book) {
-  return { 
+  return {
     type: ADD_BOOK,
-    id: nextBook++,
-    book
+    id: uuidv4(),
+    book,
   };
 }
 
 export function removeBook(id) {
-  return { 
+  return {
     type: REMOVE_BOOK,
     id,
-   };
-}
-
-// side effects, only as applicable
-// e.g. thunks, epics, etc
-export function getBooks () {
-  return dispatch => get('/books').then(book => dispatch(updateBook(book)))
+  };
 }
