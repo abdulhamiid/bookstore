39158ced1bd2262270c0f438054d46ca64e2320f
Abdulhamid <adioabdulhamid@yahoo.com>
2022-07-16 10:51:55 +0100

    Add user icon and link to the header

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BsFillPersonFill } from 'react-icons/bs';
 import {
   Link,
 } from 'react-router-dom';
@@ -6,15 +7,22 @@ import {
 function Nav() {
   return (
     <nav>
-      <h1>Bookstore CMS</h1>
-      <ul className="navbar">
-        <li>
-          <Link to="/">BOOKS</Link>
-        </li>
-        <li>
-          <Link to="/categories">CATEGORIES</Link>
-        </li>
-      </ul>
+      <div>
+        <header>
+          <h1><Link to="/">Bookstore CMS</Link></h1>
+        </header>
+        <ul className="navbar">
+          <li>
+            <Link to="/">BOOKS</Link>
+          </li>
+          <li>
+            <Link to="/categories">CATEGORIES</Link>
+          </li>
+        </ul>
+      </div>
+      <div id="user">
+        <BsFillPersonFill />
+      </div>
     </nav>
   );
 }
